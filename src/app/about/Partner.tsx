f533009d57c43dac9a05cefb05d259f7f40f19c2
edluo1 <6147723+edluo1@users.noreply.github.com>
2026-06-02 21:29:43 -0700

    Smaller width for mobile

--- src/app/about/Partner.tsx
+++ src/app/about/Partner.tsx
@@ -12,12 +12,12 @@ export default function Partner(props: PartnerProps) {
         <span className="flex-initial">
             <Link href={props.url}>
                 <Image 
-                    width={200} 
+                    width={130} 
                     height={0} 
                     src={props.image} 
                     alt={props.name} 
                     title={props.name}
-                    className="object-scale-down min-w-60 max-h-40" />
+                    className="object-scale-down min-w-30 max-h-20 md:min-w-60 md:max-h-40" />
             </Link>
         </span>
     )
