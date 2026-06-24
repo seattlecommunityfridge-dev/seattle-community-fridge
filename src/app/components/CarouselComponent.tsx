b4e0e0d021beed372993e637201fb68148547838
edluo1 <6147723+edluo1@users.noreply.github.com>
2026-06-23 23:47:06 -0700

    Adjusted height for larger screens

--- src/app/components/CarouselComponent.tsx
+++ src/app/components/CarouselComponent.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export function Component() {
   return (
-    <div className="h-[18rem] sm:h-[25rem] md:h-[32rem] w-full">
+    <div className="h-[18rem] sm:h-[25rem] md:h-[32rem] lg:h-[30rem] xl:h-[39rem] 2xl:h-[48rem] w-full">
       <Carousel className="h-full">
         <Image width={1000} height={0} src={"/slideshow/CapHill.png"} alt="Capitol Hill fridge." />
         <Image width={1000} height={0} src={"/slideshow/Estelitas.png"} alt="Estelitas fridge in Central District." />
