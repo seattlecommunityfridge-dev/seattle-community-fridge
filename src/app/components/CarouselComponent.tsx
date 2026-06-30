db5127b78c26511222a3e9ffe7280ed717fc2342
edluo1 <6147723+edluo1@users.noreply.github.com>
2026-06-30 15:28:38 -0700

    Made images preloaded to avoid load issues

--- src/app/components/CarouselComponent.tsx
+++ src/app/components/CarouselComponent.tsx
@@ -5,10 +5,10 @@ export function Component() {
   return (
     <div className="h-[18rem] sm:h-[25rem] md:h-[32rem] lg:h-[30rem] xl:h-[39rem] 2xl:h-[48rem] w-full">
       <Carousel className="h-full">
-        <Image priority width={800} height={0} sizes="100vw, (min-width:48rem) 50vw" src={"/slideshow/CapHill.png"} alt="Capitol Hill fridge." />
-        <Image width={800} height={0} sizes="100vw, (min-width:48rem) 50vw" src={"/slideshow/Estelitas.png"} alt="Estelitas fridge in Central District." />
-        <Image width={800} height={0} sizes="100vw, (min-width:48rem) 50vw" src={"/slideshow/Phinney.png"} alt="Phinney Ridge fridge." />
-        <Image width={800} height={0} sizes="100vw, (min-width:48rem) 50vw" src={"/slideshow/Burien.png"} alt="Burien fridge." />
+        <Image preload width={800} height={0} sizes="100vw, (min-width:48rem) 50vw" src={"/slideshow/CapHill.png"} alt="Capitol Hill fridge." />
+        <Image preload width={800} height={0} sizes="100vw, (min-width:48rem) 50vw" src={"/slideshow/Estelitas.png"} alt="Estelitas fridge in Central District." />
+        <Image preload width={800} height={0} sizes="100vw, (min-width:48rem) 50vw" src={"/slideshow/Phinney.png"} alt="Phinney Ridge fridge." />
+        <Image preload width={800} height={0} sizes="100vw, (min-width:48rem) 50vw" src={"/slideshow/Burien.png"} alt="Burien fridge." />
       </Carousel>
     </div>
   );
