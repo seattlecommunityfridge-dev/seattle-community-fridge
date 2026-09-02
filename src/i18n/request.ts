3360bac2f565995ae610c4fd060c46cdbbc20423
edluo1 <6147723+edluo1@users.noreply.github.com>
2026-09-02 15:56:55 -0700

    Added cookie

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,8 +1,9 @@
+import {cookies} from 'next/headers';
 import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async () => {
-	// Static for now
-	const locale = 'en';
+	const store = await cookies();
+	const locale = store.get('locale')?.value || 'en';
 
 	return {
 		locale,
